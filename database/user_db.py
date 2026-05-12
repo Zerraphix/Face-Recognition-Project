8a9c2e6e25a45f29fb222bbd84194498b76f8b94
Zerraphix <88774528+Zerraphix@users.noreply.github.com>
2026-05-12 11:54:14 +0200

    Login avaible

--- database/user_db.py
+++ database/user_db.py
@@ -62,6 +62,38 @@ def get_user_by_id(user_id):
         if conn:
             conn.close()
 
+def get_user_by_email(email):
+    conn = None
+
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+
+        cur.execute("""
+            SELECT 
+                user.user_id,
+                user.first_name,
+                user.last_name,
+                user.email,
+                user.password_hash,
+                user.role_id,
+                role.role_name
+            FROM user
+            INNER JOIN role ON user.role_id = role.role_id
+            WHERE user.email = ?
+        """, (email,))
+
+        row = cur.fetchone()
+
+        if row is None:
+            return None
+
+        return dict(row)
+
+    finally:
+        if conn:
+            conn.close()
+
 
 def create_user(first_name, last_name, email, password_hash, role_id):
     conn = None
